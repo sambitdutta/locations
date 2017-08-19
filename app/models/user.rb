90f29a510b8d828e0c5facc5211e4509357b2bb5
Sambit (Laptop) <sambit.dutta@globallogic.com>
2017-08-20 01:28:44 +0530

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+    :rememberable, :trackable, :validatable, :authentication_keys => [:login]
   
   acts_as_follower
   acts_as_followable
